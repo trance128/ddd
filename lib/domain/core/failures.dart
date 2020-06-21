dae26319c1f8c90fce2e407e6d77c9078848a65c
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-21 18:04:50 +0100

    Adding domain layer, value_objects

---
+++ lib/domain/core/failures.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'failures.freezed.dart';
+
+@freezed
+abstract class ValueFailure<T> with _$ValueFailure<T> {
+  const factory ValueFailure.invalidEmail({
+    @required String failedValue,
+  }) = InvalidEmail<T>;
+  const factory ValueFailure.shortPassword({
+    @required String failedValue,
+  }) = ShortPassword<T>;
+}
+
+class InvalidEmailException implements Exception {
+  final String failedValue;
+
+  InvalidEmailException({@required this.failedValue});
+}
