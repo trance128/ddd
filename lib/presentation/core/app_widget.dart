db01ac4afc74b315a7a9e3f3f1cabe5ac781778d
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-22 21:05:47 +0100

    sign_in_form page

---
+++ lib/presentation/core/app_widget.dart
@@ -0,0 +1,22 @@
+import 'package:ddd/presentation/sign_in/sign_in_page.dart';
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes',
+      home: SignInPage(),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light().copyWith(
+        primaryColor: Colors.blueGrey[800],
+        accentColor: Colors.blueAccent,
+        inputDecorationTheme: InputDecorationTheme(
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
