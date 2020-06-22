ede5d450bfea6e748fd0c5f0c75de755cb58cb46
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-22 17:38:32 +0100

    firebase_auth_facade

--- lib/domain/core/value_object.dart
+++ lib/domain/core/value_object.dart
@@ -1,12 +1,23 @@
 import 'package:dartz/dartz.dart';
-import 'package:ddd/domain/core/failures.dart';
 import 'package:flutter/foundation.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'errors.dart';
+import 'failures.dart';
 
 @immutable
 abstract class ValueObject<T> {
   const ValueObject();
   Either<ValueFailure<T>, T> get value;
+
+  /// Throws [UnexpectedValueError] containing the [ValueFailure] or 
+  /// returns the value
+  T getOrCrash() {
+    // id = identity - same as writing (r) => r;
+    return value.fold((failure) => throw UnexpectedValueError(failure), id);
+  }
+
+  bool isValid() => value.isRight();
 
   @override
   bool operator ==(Object o) {
