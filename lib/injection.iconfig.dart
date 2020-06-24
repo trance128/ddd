176ebc331e46189cc9be84bb4b2b35a1408dbf34
Ovidius Mazuru <ovidiu.mazuru@gmail.com>
2020-06-24 13:35:39 +0100

    note blocs

--- lib/injection.iconfig.dart
+++ lib/injection.iconfig.dart
@@ -9,6 +9,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:ddd/infrastructure/auth/firebase_auth_facade.dart';
 import 'package:ddd/domain/auth/i_auth_facade.dart';
+import 'package:ddd/application/notes/note_watcher/note_watcher_bloc.dart';
+import 'package:ddd/domain/notes/i_note_repository.dart';
 import 'package:ddd/application/auth/sign_in_form/sign_in_form_bloc.dart';
 import 'package:ddd/application/auth/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
@@ -21,6 +23,8 @@ void $initGetIt(GetIt g, {String environment}) {
       () => fireBaseInjectableModule.googleSignIn);
   g.registerLazySingleton<IAuthFacade>(
       () => FirebaseAuthFacade(g<FirebaseAuth>(), g<GoogleSignIn>()));
+  g.registerFactory<NoteWatcherBloc>(
+      () => NoteWatcherBloc(g<INoteRepository>()));
   g.registerFactory<SignInFormBloc>(() => SignInFormBloc(g<IAuthFacade>()));
   g.registerFactory<AuthBloc>(() => AuthBloc(g<IAuthFacade>()));
 }
